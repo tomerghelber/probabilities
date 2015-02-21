795f99f3dca5e1f4784513c8c14a0c8acddc21e8
tomer gelber <tomergelber@gmail.com>
2015-02-21 18:41:51 +0200

    exceptions doc is working

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -14,12 +14,14 @@
 # serve to show the default.
 
 import sys
-import os
+from os import path
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+__file_dir__ = path.dirname(__file__)
+sys.path.insert(0, path.abspath(path.join(__file_dir__, '..', '..', 'probability')))
+
 
 # -- General configuration ------------------------------------------------
 
--- sphinx/source/objects/exceptions/index.rst
+++ sphinx/source/objects/exceptions/index.rst
@@ -3,14 +3,9 @@ Exceptions
 All the exceptions in probabilities are placed here.
 Create a new file for each large exception - if it got inheritance or some exception that walking together.
 
-ProbabilityError
-----------------
 .. automodule:: exceptions
 .. autoclass:: ProbabilityError
    :show-inheritance:
 
-ProbabilityValueError
----------------------
-.. automodule:: exceptions
 .. autoclass:: ProbabilityValueError
    :show-inheritance:
