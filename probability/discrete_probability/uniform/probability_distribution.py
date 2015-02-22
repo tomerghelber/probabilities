d123ed13660c4e93a6038461dc540c45cbe54257
tomer gelber <tomergelber@gmail.com>
2015-02-22 18:02:41 +0200

    removed doc_inherit - not working with properties

--- probability/discrete_probability/uniform/probability_distribution.py
+++ probability/discrete_probability/uniform/probability_distribution.py
@@ -1,6 +1,5 @@
 from probability.discrete_probability.uniform import probability
 from probability.discrete_probability.base import probability_distribution
-from probability.doc_inherit import doc_inherit
 
 
 class UniformDiscreteProbabilityDistribution(probability_distribution.DiscreteProbabilityDistribution):
@@ -10,32 +9,26 @@ class UniformDiscreteProbabilityDistribution(probability_distribution.DiscretePr
         self.a = a
         self.b = b
 
-    @doc_inherit
     @property
     def _probability_class(self):
         return probability.UniformDiscreteProbability
 
-    @doc_inherit
     @property
     def expected_value(self):
         return 1 / (self.b - self.a)
 
-    @doc_inherit
     @property
     def median(self):
         return (self.b + self.a) / 2.0
 
-    @doc_inherit
     @property
     def mode(self):
         return self.expected_value
 
-    @doc_inherit
     @property
     def variance(self):
         return 0
 
-    @doc_inherit
     def _get(self, value):
         if self.a < value < self.b:
             return self._probability_class(self.expected_value)
