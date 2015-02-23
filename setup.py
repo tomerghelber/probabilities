75d9c84dcc963a2881f7351b70181ab409980cfc
tomer gelber <tomergelber@gmail.com>
2015-02-23 12:49:54 +0200

    added version, keywords and changed the name of the package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
 
-NAME = "probability"
+NAME = "probabilities"
 
 # Dynamically calculate the version based on __version__.
 version = __import__(NAME).__version__
@@ -12,18 +12,18 @@ test_requirements_path = "test_requirements.txt"
 
 
 def get_requirements(path):
+    # parse_requirements() returns generator of pip.req.InstallRequirement objects
     return map(lambda ir: str(ir.req), parse_requirements(path))
 
 
 setup(
     name=NAME,
-    packages=find_packages(exclude=['tests*']),
+    packages=find_packages(exclude=['tests*', 'sphinx*']),
     version=version,
-    description='probability',
+    description='probabilities',
     author='user',
     license="BSD",
-    keywords=["probability"],
-    # parse_requirements() returns generator of pip.req.InstallRequirement objects
+    keywords=["probabilities", "probability"],
     install_requires=get_requirements(requirements_path),
     tests_require=get_requirements(test_requirements_path),
 )
